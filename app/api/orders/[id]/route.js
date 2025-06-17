1b438a2fe3de99b00b8ec31b35ce69e5c73e8c8d
luke0w0 <aa0926337891@gmail.com>
2025-06-17 15:16:24 +0800

    no vercel

--- app/api/orders/[id]/route.js
+++ app/api/orders/[id]/route.js
@@ -1,7 +1,7 @@
 import { NextResponse } from "next/server";
 import { getServerSession } from "next-auth";
 import { authOptions } from "@/app/api/auth/[...nextauth]/route";
-import { prisma } from '@/lib/prisma';
+import prisma from '@/lib/prisma';
 
 // 獲取特定訂單
 export async function GET(request, { params }) {
